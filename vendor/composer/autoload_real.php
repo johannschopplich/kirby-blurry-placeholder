aa14929d69fde4ad91cf4221a9fd5fd6fa09116b
Johann Schopplich <mail@johannschopplich.com>
2021-03-04 21:25:34 +0100

    Upgrade Kirby composer installer

--- vendor/composer/autoload_real.php
+++ vendor/composer/autoload_real.php
@@ -25,7 +25,7 @@ class ComposerAutoloaderInit200c1adf736762912fd73724e465053c
         require __DIR__ . '/platform_check.php';
 
         spl_autoload_register(array('ComposerAutoloaderInit200c1adf736762912fd73724e465053c', 'loadClassLoader'), true, true);
-        self::$loader = $loader = new \Composer\Autoload\ClassLoader();
+        self::$loader = $loader = new \Composer\Autoload\ClassLoader(\dirname(\dirname(__FILE__)));
         spl_autoload_unregister(array('ComposerAutoloaderInit200c1adf736762912fd73724e465053c', 'loadClassLoader'));
 
         $useStaticLoader = PHP_VERSION_ID >= 50600 && !defined('HHVM_VERSION') && (!function_exists('zend_loader_file_encoded') || !zend_loader_file_encoded());
--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -6,19 +6,19 @@
     'aliases' => 
     array (
     ),
-    'reference' => 'adb1300732a80553753a6326def4afd15d11ec22',
+    'reference' => '5b55f8c37f8c65c9333ec0c246074e1158d7baf9',
     'name' => 'johannschopplich/kirby-blurry-placeholder',
   ),
   'versions' => 
   array (
     'getkirby/composer-installer' => 
     array (
-      'pretty_version' => '1.2.0',
-      'version' => '1.2.0.0',
+      'pretty_version' => '1.2.1',
+      'version' => '1.2.1.0',
       'aliases' => 
       array (
       ),
-      'reference' => '240a8b2c275d61b66601feb58222b7d34bc6cf1e',
+      'reference' => 'c98ece30bfba45be7ce457e1102d1b169d922f3d',
     ),
     'johannschopplich/kirby-blurry-placeholder' => 
     array (
@@ -27,7 +27,7 @@
       'aliases' => 
       array (
       ),
-      'reference' => 'adb1300732a80553753a6326def4afd15d11ec22',
+      'reference' => '5b55f8c37f8c65c9333ec0c246074e1158d7baf9',
     ),
   ),
 );

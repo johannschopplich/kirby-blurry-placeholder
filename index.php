ab3b3a503701387ffd86c5f050b54b1a8b488d0e
tobimori <tobias@moeritz.cc>
2023-01-05 18:07:30 +0100

    feat: transparent placeholders option (closes #5)

--- index.php
+++ index.php
@@ -8,11 +8,11 @@ load([
 
 \Kirby\Cms\App::plugin('johannschopplich/blurry-placeholder', [
     'fileMethods' => [
-        'placeholder' => function (float|null $ratio = null) {
-            return BlurryPlaceholder::image($this, $ratio);
+        'placeholder' => function (float|null $ratio = null, bool|null $transparent = null) {
+            return BlurryPlaceholder::image($this, $ratio, $transparent);
         },
-        'placeholderUri' => function (float|null $ratio = null) {
-            return BlurryPlaceholder::uri($this, $ratio);
+        'placeholderUri' => function (float|null $ratio = null, bool|null $transparent = null) {
+            return BlurryPlaceholder::uri($this, $ratio, $transparent);
         }
     ],
     'tags' => [
